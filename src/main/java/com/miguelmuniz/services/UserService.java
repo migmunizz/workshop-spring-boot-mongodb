60e1cb08c274a2aa9f14b96f93da4eda9415ec18
Miguel Muniz <mr.muniz4230@gmail.com>
2026-06-18 10:22:51 -0300

    Connecting to MongoDB with repository and service

---
+++ src/main/java/com/miguelmuniz/services/UserService.java
@@ -0,0 +1,19 @@
+package com.miguelmuniz.services;
+
+import com.miguelmuniz.domain.User;
+import com.miguelmuniz.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+}

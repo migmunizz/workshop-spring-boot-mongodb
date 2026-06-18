98ce8e057ef352fb2aef0b94db8fc5afdaed466f
Miguel Muniz <mr.muniz4230@gmail.com>
2026-06-18 08:55:00 -0300

    Entity User

---
+++ src/main/java/com/miguelmuniz/domain/User.java
@@ -0,0 +1,57 @@
+package com.miguelmuniz.domain;
+
+import java.util.Objects;
+
+public class User {
+    private String id;
+    private String name;
+    private String email;
+
+
+    public User() {
+    }
+
+    public User(String id, String name,String email ) {
+        this.email = email;
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}

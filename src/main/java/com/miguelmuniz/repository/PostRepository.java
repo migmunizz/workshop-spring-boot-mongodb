a7bd0fa3575facdb3889d5266f210d64e53cd07f
Miguel Muniz <mr.muniz4230@gmail.com>
2026-06-23 19:27:17 -0300

    simple consult with @Query

--- src/main/java/com/miguelmuniz/repository/PostRepository.java
+++ src/main/java/com/miguelmuniz/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.miguelmuniz.repository;
 import com.miguelmuniz.domain.Post;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +13,6 @@ public interface PostRepository extends MongoRepository<Post,String> {
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String text);
 }
